2442ad09b7d4fe36486f871227edd01f484f2af0
Keith Walker <Keithrwalker418@gmail.com>
2019-06-24 18:53:01 -0500

    setup home/auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Auth from '../components/Auth/Auth';
+import Home from '../components/Home/Home';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <h1>FISH STORE 06.24.2019</h1>
-      <button className='btn btn-danger'>Fish Store</button>
+      <Auth />
+      <Home />
     </div>
   );
 }
