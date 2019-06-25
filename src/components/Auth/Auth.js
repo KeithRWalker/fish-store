a19e42f580eafd9c57fc9b0c6ea100e6070119a0
Keith Walker <Keithrwalker418@gmail.com>
2019-06-24 19:13:42 -0500

    helpers

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -8,6 +8,7 @@ class Auth extends React.Component {
       <div className="Auth">
         <h1>FISH STORE 06.24.2019:</h1>
         <h1>Auth</h1>
+        <button className="auth-btn btn btn-secondary">Login With Google</button>
       </div>
     );
   }
