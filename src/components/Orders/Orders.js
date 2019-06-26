5446f7856049696e6320226741c9c61c218beb4c
Keith Walker <Keithrwalker418@gmail.com>
2019-06-25 21:26:03 -0500

    table setup

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import ordersData from '../../helpers/data/ordersData';
+import OrderRow from '../OrderRow/OrderRow';
 
 import './Orders.scss';
 
 class Orders extends React.Component {
+  state = {
+    orders: [],
+  }
+
+  componentDidMount() {
+    ordersData.getMyOrders(firebase.auth().currentUser.uid)
+      .then(orders => this.setState({ orders }))
+      .catch(err => console.error('error', err));
+  }
+
   render() {
+    const orderComponents = this.state.orders.map(order => (
+      <OrderRow key={order.id} order={order} />
+    ));
+
     return (
       <div className="Orders">
-        <h5>Orders</h5>
+        <h2>Orders</h2>
+        <table className="table order-table">
+          <thead>
+            <tr>
+              <th scope="col">Order #</th>
+              <th scope="col">Date</th>
+              <th scope="col"># Fish</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {orderComponents}
+          </tbody>
+        </table>
       </div>
     );
   }
