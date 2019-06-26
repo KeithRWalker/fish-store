c61c2eb84878b76b12215b64e56c833a60177157
Keith Walker <Keithrwalker418@gmail.com>
2019-06-25 20:59:15 -0500

    inventory done

--- src/components/Fish/fish.js
+++ src/components/Fish/fish.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import fishShape from '../../helpers/propz/fishShapes';
+import format from '../../helpers/format';
 
 import './fish.scss';
 
@@ -11,12 +12,30 @@ class Fish extends React.Component {
 
   render() {
     const { fish } = this.props;
-    const altTxt = `${fish.name} ${fish.desc}`;
+    const isAvailable = fish.status === 'available';
+    // eslint-disable-next-line
+    const image = require(`${fish.image}`)
     return (
       <li className="Fish">
-        <img className="image" src={fish.image} alt={altTxt}></img>
-        <h3 className="name">{fish.name}</h3>
-        <p className="desc">{fish.desc}</p>
+
+        <img src={image} alt={fish.name} />
+
+        <h3 className="name">
+
+          {fish.name}
+
+          <span className="price">{format.formatPrice(fish.price)}</span>
+
+        </h3>
+
+        <p>{fish.desc}</p>
+
+        <button className="btn btn-danger"
+          disabled={!isAvailable}
+        >
+          {isAvailable ? 'Add To Order' : 'Sold Out!'}
+        </button>
+
       </li>
     );
   }
